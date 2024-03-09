07c1affcc6f414727c2ea8cd53e29a1378d65ac5
Dinis Machado <dinisma@gmail.com>
2024-03-09 03:32:16 +0000

    Istall Summernote text editor dependency

--- job_manager/admin.py
+++ job_manager/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Job
+from django_summernote.admin import SummernoteModelAdmin
+
 # Register your models here.
-admin.site.register(Job)
+
+# admin.site.register(Job)
+@admin.register(Job)
+class JobAdmin(SummernoteModelAdmin):
+    list_display = ('job_description', 'slug', 'status')
+    search_fields = ('job_description',)
+    list_filter = ('freelance_hire', 'responsibility')
+    summernote_fields = ('job_description')
+    prepopulated_fields = {'slug': ('job_to_apply',)}
+
